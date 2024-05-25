5ae0d910a186c731bf72c8a586b19956c2f9f41f
HaagHenrique <henriquehaag11@gmail.com>
2024-05-25 17:32:37 -0300

    Exercício 1 da primeira lista de exercícios dentro da aula 2 feita

--- Aula_02/Seq_d_int.c
+++ Aula_02/Seq_d_int.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <math.h>
+
+// Esse código precisou da seguinte command line para funcionar:
+// gcc -o Seq_d_int Seq_d_int.c -lm
+// O "-lm" foi necessário para estabelecer o link com a biblioteca math.h. 
+// Ainda não entendi completamente o motivo/razão da biblioteca não funcionar sozinha, 
+// mas é isso que conserta a situação.
 
 int main ()
 {
@@ -8,15 +15,19 @@ int main ()
     int min;
     float media;
     float soma;
-    
+    double desvio_padrao;
+    double somatorio;
+
     printf("Escreva a quantidade de valores a serem escritos: ");
     scanf("%d", &qtd);
+    int lista_numeros[qtd];
 
     for (int i = 0; i < qtd; i++)
     {
         printf("Escreva aqui o valor: ");
         scanf("%d", &digit);
         soma += digit;
+        lista_numeros[i] = digit;
 
         if (i == 0)
         {
@@ -32,10 +43,17 @@ int main ()
 
     media = soma / qtd;
 
+    for (int i = 0; i < qtd; i++)
+    {
+        somatorio += pow (lista_numeros[i] - (double) media, 2);
+    }
+    double intermediario = somatorio / (double) qtd;
+    desvio_padrao = sqrt(intermediario);
+
     printf("O valor máximo digitado foi %d\n", max);
     printf("O valor mínimo digitado foi %d\n", min);
-    printf("A média dos valores é %f\n", media);
-    // Ainda preciso calcular o desvio padrão? https://pt.khanacademy.org/math/statistics-probability/summarizing-quantitative-data/variance-standard-deviation-population/a/calculating-standard-deviation-step-by-step
+    printf("A média dos valores é %.2f\n", media);
+    printf("O desvio padrão (real) de toda a população de números é %.2f\n", desvio_padrao);
 
     return (0);
 }

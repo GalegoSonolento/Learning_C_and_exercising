df773ea8e8f584a0d6dadbf5263b717003f6182f
vboxuser <vboxuser@OutroTeste.myguest.virtualbox.org>
2024-07-25 00:06:41 -0300

    Tentativa de anotação e continuidade nos exercícios

---
+++ Aula_03/Questao_1_verificando_se_num_eh_triangular.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void solve() 
+{
+    //Esse deve ser meu código para 
+    // identificação de número triangular
+    int a;
+
+    scanf ("%d", &a);
+
+    int aux = 0;
+}
+
+int main ()
+{
+    solve();
+    return 0;
+}
